266d7333632f9b857811567ccc1a8512f4e12f07
Michael Gilson <57264261+MichaelJGilson@users.noreply.github.com>
2021-10-20 20:04:32 -0700

    Update DoctorNurse.java

--- DoctorNurse.java
+++ DoctorNurse.java
@@ -1,1 +1,22 @@
+// Header for the Doctor and Nurse Class File
 
+//Doctor Class creation
+/* Doc class will include the following functions and vars:
+
+DoctorID - int
+
+functions:
+
+login(using doctorID)
+
+  opens the javaFX login page
+  asks user for id
+  
+  checks if id is correct
+  
+    if true
+      allow user access to adminPanel
+    if false
+      print error
+
+*/
