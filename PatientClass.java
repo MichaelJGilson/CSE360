fd9375f7d204957833034e07d1edad415b774def
ssing321 <81613102+ssing321@users.noreply.github.com>
2021-10-24 17:50:59 -0700

    Update PatientClass.java

--- PatientClass.java
+++ PatientClass.java
@@ -1,1 +1,21 @@
 
+// Header for the Patient  Class File
+
+//Patient Class creation
+/* Patient class will include the following functions and vars:
+name - string
+dateOfBirth - int
+phoneNumber - int 
+email - string
+dateofVisit - int
+
+functions:
+
+      
+ messageDoctor():
+  opens javaFX messaging page where the Doctor can select patient to view message history and send messages
+  
+ printRecord():
+  patient previous medical records like their medications and immunizations would be stored here.
+
+*/
