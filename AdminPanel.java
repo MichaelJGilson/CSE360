87a2ebf894a66dc470edd6185776c2b8b5e71f73
Michael Gilson <57264261+MichaelJGilson@users.noreply.github.com>
2021-10-24 19:39:33 -0700

    Update AdminPanel.java

--- AdminPanel.java
+++ AdminPanel.java
@@ -1,5 +1,13 @@
 // Header for the Admin Panel
 
+//function to read file of list of admins (docs/nurses)
+/* 
+while (each line contains a doctor ID of integers)
+  read in integers from files containing doctor ID's
+  create a new object with the key doctorID (int) being equal to the read value
+close file
+*/
+
 // Admin Panel Class creation
 /*
 LogIn ID - int
@@ -12,3 +20,13 @@ if True
   print "Login Successful"
 Else
   print "Login Failed"
+
+once logged in, using javafx - the admin panel will display the following panes:
+
+display Admin pane which includes:
+  nurse tab
+  doctor tab
+    message button
+    upload medical record button
+  print button
+  and the options menu
