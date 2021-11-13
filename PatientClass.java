1dcd398fb8fa87f3eb42e8e0acbc22fbaa8bc830
Alan H <78623598+alanhafeed@users.noreply.github.com>
2021-11-13 12:33:16 -0700

    Update PatientClass.java

--- PatientClass.java
+++ PatientClass.java
@@ -1,21 +1,49 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 
 // Header for the Patient  Class File
 
-//Patient Class creation
-/* Patient class will include the following functions and vars:
-name - string
-dateOfBirth - int
-phoneNumber - int 
-email - string
-dateofVisit - int
-
-functions:
-
-      
- messageDoctor():
-  opens javaFX messaging page where the Doctor can select patient to view message history and send messages
-  
- printRecord():
-  patient previous medical records like their medications and immunizations would be stored here and can be printed if needed.
-
-*/
+public class PatientClass extends User{
+	//Pharmacy pharmacy;
+	//ArrayList<MedHist> medicalHistory;
+	DoctorNurse associatedDoctorNurse;
+	ArrayList<String> messageList;
+	
+	public PatientClass(String name, String email, String phoneNum, String address, String password, String dateOfBirth, String dateOfVisit, Pharmacy pharmacyName, DoctorNurse docNurse){
+		super(name, email, phoneNum, address, password, dateOfBirth, dateOfVisit);
+		//pharmacy = pharmacyName;
+		associatedDoctorNurse = docNurse;
+		//medicalHistoryList = new ArrayList<MedHist>();
+	}
+	
+	//getters
+	
+	/*
+	public Pharmacy getPharmacy(){
+		return pharmacy;
+	}
+	*/
+	
+	public Doctor getDoctor(){
+		return associatedDoctorNurse;
+	}
+	
+	public ArrayList<String> getMessageList(){
+		return messageList;
+	}
+	
+	//acts as an inbox of messages 
+	public void messageInbox(ArrayList<String> inbox){
+		messageList = inbox;
+	}
+	
+	//opens javaFX messaging page where the Doctor can select patient to view message history and send messages
+	public void messageDoctor(String message){
+		messageList.add(message);
+	}
+	
+	//patient previous medical records like their medications and immunizations would be stored here and can be printed if needed.
+	public static void printRecord(){
+		
+	}
+}
